f38f8afdf69da9e25f9a702749b64a71210f0e38
accxpz <53883255+accxpz@users.noreply.github.com>
2021-05-05 20:49:08 +0800

    Add files via upload

---
+++ Staff-Managment-System/Sprint1/login.php
@@ -0,0 +1,55 @@
+<?php require_once('includes/functions.php'); ?>
+<?php
+    $errors = [];
+    if(isset($_POST['login'])) {
+        $errors = loginUser($_POST);
+
+        if(count($errors) === 0) {
+	echo "<script>alert('Login success');parent.location.href='manage.php';</script>";
+            exit();
+        }
+    }
+?>
+<?php
+    $errors = [];
+    if(isset($_POST['login'])) {
+        $errors = loginStaff($_POST);
+
+        if(count($errors) === 0) {
+	echo "<script>alert('Login success');parent.location.href='manage.php';</script>";
+            exit();
+        }
+    }
+?>
+<!DOCTYPE html><html lang="en">
+<head>
+        <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+
+    <div class="container">
+    <h1>Login</h1>
+        <div class="row">
+                <form method="post">
+                    <div class="user">
+                <label for="secondname">Email</label>
+                <input type="text" id="email" name="email"
+                <?php displayValue($_POST, 'email'); ?> />
+                <?php displayError($errors, 'email'); ?>
+                </div>
+					<!-- Verify email and password  -->
+                <div class="user">
+                <label>Password</label>
+                <input type="password"id="password" name="password" />
+                <?php displayError($errors, 'password'); ?>
+                </div>
+                <button type="submit" class="btnlogin" name="login" value="login">Login</button>
+                
+            </form>
+        </div>
+        <a href="register.php"><button type="submit" name="register" value="register">Register Now</button></a>
+		
+    </div>
+</body>
+</html>
---
+++ Staff-Managment-System/Sprint1/register.php
@@ -0,0 +1,78 @@
+<?php require_once('includes/functions.php'); ?>
+<?php
+    $errors = [];
+    if(isset($_POST['register'])) {
+        $errors = registerUser($_POST);
+
+        if(count($errors) === 0) {
+            header('Location: login.php');
+            exit();
+        }
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Register</title>
+        <link rel="stylesheet" href="css/breadcrumbs.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="javaScript/jquery.breadcrumbs-generator.js"></script>
+        <script>
+            $(function() {
+              $('#breadcrumbs').breadcrumbsGenerator();
+            });
+            </script>
+</head>
+<body>
+    <!--header-->
+        
+            <!--registeration form-->
+                <div id="register-content">
+                <h1 style="text-align: center;margin-top: 10px;">Register Now!</h1>
+                <form class="registration_form" method="post">
+                    <div>
+                        <label for="firstname">First name:</label>
+                        <input type="text" id="firstname" name="firstname"
+                            <?php displayValue($_POST, 'firstname'); ?> />
+                        <?php displayError($errors, 'firstname'); ?>
+                    </div>
+
+                    <div>
+                        <label>Last name:</label>
+                        <input type="text" id="lastname" name="lastname"
+                            <?php displayValue($_POST, 'lastname'); ?> />
+                        <?php displayError($errors, 'lastname'); ?>
+                    </div>
+
+
+                    <div>
+                        <label>Email:</label>
+                        <input type="text" id="email" name="email"
+                            <?php displayValue($_POST, 'email'); ?> />
+                        <?php displayError($errors, 'email'); ?>
+                    </div>
+
+                    <div>
+                        <label>Password:</label>
+                        <input type="password" id="password" name="password" />
+                        <?php displayError($errors, 'password'); ?>
+                    </div>
+
+                    <div>
+                        <label>Confirm password:</label>
+                        <input type="password" id="confirmPassword" name="confirmPassword" />
+                        <?php displayError($errors, 'confirmPassword'); ?>
+                    </div>
+
+                    
+
+                    <button type="submit" class="submit" name="register" value="register">Register</button>
+                </form>
+        </div>
+
+            <!--footer-->
+
+         
+</body>
+</html>
